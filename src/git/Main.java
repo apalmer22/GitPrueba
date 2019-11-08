d9db901e4aa255784c22787ecc5dc7f3aafb4e32
Arnau Palmer <apalmer2@nigul.cide.es>
2019-11-08 20:39:45 +0100

    editamos por segunada vez

--- src/git/Main.java
+++ src/git/Main.java
@@ -1,10 +1,10 @@
 package git;
 
-public class Main {
-
-	public static void main(String[] args) {
-		System. out .println("Hello World!");
-
-	}
-
+	public class Main {
+			public static void main(String[] args) {
+			printHola();
+			}
+			public static void printHola(){
+			System.out.println("Hola món!");
+}
 }
